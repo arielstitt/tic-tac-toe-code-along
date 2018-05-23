b8179160f0ea1758a6c604c9472692ed6d198841
arielstitt <arielstitt@gmail.com>
2018-05-23 10:08:17 -0400

    added game over function

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ function turn(squareId, player) {
 
 function checkWin(board, player) {
     let plays = board.reduce((a, e, i) => 
-    (e === player)) ? a.concat(i) : a, []
+    (e === player) ? a.concat(i) : a, [])
     let gameWon = null
     for (let [index, win] of winCombos.entries()) {
         if (win.every(elem => plays.indexOf(elem) > -1)){
@@ -61,11 +61,14 @@ function checkWin(board, player) {
     }
     return gameWon
 }
-
+//============GAME OVER ===============//
 function gameOver(gameWon) {
     for( let index of winCombos[gameWon.index]){
-        document.getElementById(index).style.backgroundColor = blue;
+        document.getElementById(index).style.backgroundColor = 
         gameWon.player == huPlayer ? 'blue' : 'red'
+    }
+    for (var i = 0; i < cells.length; i ++){
+        cells[i].removeEventListener('click', turnClick, false)
     }
 }
 //determine a winner
