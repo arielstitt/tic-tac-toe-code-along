ad3aa82a21b4e545b5344b9b4ae0791a0c62a322
arielstitt <arielstitt@gmail.com>
2018-05-23 10:34:45 -0400

    added a bestSpot, DdeclareWinner function

--- app.js
+++ app.js
@@ -40,7 +40,12 @@ function startGame() {
 }
 
 function turnClick(square) {
-    turn(square.target.id, huPlayer)
+    if (typeof origBoard[square.target.id] == 'number') {
+        turn(square.target.id, huPlayer)
+        if (!checkTie())
+            turn(bestSpot(), aiPlayer)
+    }
+
 }
 
 function turn(squareId, player) {
@@ -51,12 +56,12 @@ function turn(squareId, player) {
 }
 
 function checkWin(board, player) {
-    let plays = board.reduce((a, e, i) => 
-    (e === player) ? a.concat(i) : a, [])
+    let plays = board.reduce((a, e, i) =>
+        (e === player) ? a.concat(i) : a, [])
     let gameWon = null
     for (let [index, win] of winCombos.entries()) {
-        if (win.every(elem => plays.indexOf(elem) > -1)){
-            gameWon = {index: index, player: player};
+        if (win.every(elem => plays.indexOf(elem) > -1)) {
+            gameWon = { index: index, player: player };
             break;
         }
     }
@@ -64,20 +69,50 @@ function checkWin(board, player) {
 }
 //============GAME OVER ===============//
 function gameOver(gameWon) {
-    for( let index of winCombos[gameWon.index]){
-        document.getElementById(index).style.backgroundColor = 
-        gameWon.player == huPlayer ? 'blue' : 'red'
+    for (let index of winCombos[gameWon.index]) {
+        document.getElementById(index).style.backgroundColor =
+            gameWon.player == huPlayer ? 'blue' : 'red'
     }
-    for (var i = 0; i < cells.length; i ++){
+    for (var i = 0; i < cells.length; i++) {
         cells[i].removeEventListener('click', turnClick, false)
     }
 }
+
+function declareWinner(who) {
+    document.querySelector('..endgame').style.display = 'block'
+    document.querySelector('.endgame .text').innerText = who
+}
+
+function emptySquares(){
+    return origBoard.filter(s => typeof s == 'number')
+}
+
+//=======BEST SPOT============//
+//this is how we're going to find the spot the ai player is going to play
+function bestSpot() {
+    return emptySquares()[0]
+}
+
+function checkTie(){
+    if (emptySquares().length == 0) {
+        for (var i = 0; i < cells.length; i ++){
+            cells[i].style.backgroundColor = "green"
+            cells[i].removeEventListener('click', turnClick, false)
+        }
+        declareWinner('tie game')
+        return true
+    }
+    return false
+}
+
 //determine a winner
 //set up logic to determine the winner
 
 
 //Basic AI and winner notification
 //notify who the winner is
+
+
 
 //minimax algorithm
 //create an unbeatable ai
